b697d48fa84a1a608ed2783ba7401d554a0e6656
Sebastian Springer <sebastian.springer@mayflower.de>
2013-10-16 17:19:12 +0200

    added jslint to grunt default tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,5 +67,5 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('default', ['clean', 'less', 'copy']);
+    grunt.registerTask('default', ['clean', 'less', 'copy', 'jslint']);
 };
