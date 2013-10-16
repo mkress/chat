3dce438638f605e71fa490c256d431d7997a5668
Sebastian Springer <sebastian.springer@mayflower.de>
2013-10-16 17:16:22 +0200

    moved js logic from jade file to js files

--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,20 @@
-/**
- * Created by sspringer on 10/16/13.
- */
+var saveHistory = function (line) {
+    if (localStorage.length > 10) {
+        for (var i = 1; i < 11; i++) {
+            localStorage.setItem(i, '');
+        }
+    }
+    for (var i = 10; i > 1; i--) {
+        localStorage.setItem(i, localStorage.getItem(i - 1));
+    }
+    localStorage.setItem(1, line);
+};
+
+var getHistory = function () {
+    for (var i = 10; i > 0; i--) {
+        var item = localStorage.getItem(i);
+        if (item !== 'null') {
+            $('#msgs').append(item);
+        }
+    }
+}
